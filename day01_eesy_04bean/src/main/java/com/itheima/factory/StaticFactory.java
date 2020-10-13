9b128c30f8140d46f7863c1eb9da6ac5a69c0f1c
luyinren <18896994109@163.com>
2020-10-13 20:30:30 +0800

    Spring中bean的细节

---
+++ day01_eesy_04bean/src/main/java/com/itheima/factory/StaticFactory.java
@@ -0,0 +1,16 @@
+package com.itheima.factory;
+
+import com.itheima.service.IAccountService;
+import com.itheima.service.impl.AccountServiceImpl;
+
+/**
+ * @Author lyr
+ * @Date 2020/10/12 22:07
+ * @Version 1.0
+ * @Description 模拟一个工厂类，该类可能存在于jar包中，我们无法通过修改源码的方式来提供默认构造函数
+ */
+public class StaticFactory {
+    public static IAccountService getAccountService() {
+        return new AccountServiceImpl();
+    }
+}
---
+++ day01_eesy_04bean/src/main/java/com/itheima/service/impl/AccountServiceImpl.java
@@ -0,0 +1,31 @@
+package com.itheima.service.impl;
+
+import com.itheima.service.IAccountService;
+
+/**
+ * @Author lyr
+ * @Date 2020/10/8 21:07
+ * @Version 1.0
+ * @Description 账户的业务层实现类
+ */
+public class AccountServiceImpl implements IAccountService {
+
+
+
+    public AccountServiceImpl(){
+        System.out.println("对象创建了");
+    }
+
+    public void saveAccount() {
+        System.out.println("service 中的saveAccount方法执行了");
+    }
+
+    public void init() {
+        System.out.println("对象初始化了。。。。。");
+    }
+
+    public void destory() {
+        System.out.println("对象销毁了。。。。。。");
+    }
+
+}
